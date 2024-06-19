3879c7140eafd9737ee38c4526bcfbe49260047d
= <=>
2024-06-19 12:18:19 +0200

    Ejercicio 6

---
+++ javascript/sheet01/ex06/main.js
@@ -0,0 +1,13 @@
+let x = saludar("Pepe")
+function saludar(nombre) {
+return "Saludos, " + nombre;
+}
+document.getElementById("holiwi").innerHTML = x;
+    function mayor(num1, num2)
+{
+        return Math.max(num1, num2)
+}
+ let a = 5;
+let b = 4;
+console.log(`El mayor entre ${a} y  ${b} es $mayor(a,b)}`);
+mayor(a,b);
