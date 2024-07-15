80942bd24079fa12e0a0d27f25c658aec0c104c2
= <=>
2024-07-15 11:35:11 +0200

    ss

---
+++ JAVA/sheet01/Encapsulation/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      Person myObj = new Person();
+      myObj.name = "John";  // error
+      System.out.println(myObj.name); // error 
+    }
+  }
+  
+  
---
+++ JAVA/sheet01/ex07/Funciones.java
@@ -0,0 +1,15 @@
+public class Funciones {
+    // Función que toma dos números enteros y devuelve su suma
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // Llamada a la función sumar con dos números enteros
+        int resultado = sumar(20, 20);
+        
+        // Muestra el resultado en la consola
+        System.out.println("La suma de 20 y 20 es: " + resultado);
+    }
+}
+
